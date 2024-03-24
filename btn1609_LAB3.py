9f471c48e076703620f6f504af5aab275f2cf442
Benjamin Niccum <113940036+Benjination@users.noreply.github.com>
2024-03-24 22:25:37 +0000

    Reverse Polish Notation

--- btn1609_LAB3.py
+++ btn1609_LAB3.py
@@ -1,1 +1,38 @@
-print("Hello, World!")
+
+#Benjamin Niccum
+#1002111609
+#03/24/24
+
+def calculate(line):
+    operators = set(['+', '-', '*', '/'])
+    stack = []
+    
+    for token in line:                   #Goes through every token in line
+        if token not in operators:       #Token is an integer
+            stack.append(int(token))     #Throw it on the stack
+        else:                            #Found first operator
+            #pop off last two integers, perform function, place result on the stack
+            b = stack.pop()               
+            a = stack.pop()
+            if token == '+':
+                stack.append(a + b)
+            elif token == '-':
+                stack.append(a - b)
+            elif token == '*':
+                stack.append(a * b)
+            elif token == '/':
+                stack.append(int(a / b))
+    
+    if len(stack) == 0:
+        return None
+    else:
+        return stack[0]
+
+# Main program
+file = "input_RPN.txt"                   #Hardcoded
+with open(file, 'r') as f:
+    for line in f:                        #Sends input one line at a time
+        line = line.replace("\n","")      #Removes Linebreak from the end of the line
+        line = line.replace(" ","")       #Removes Spaces
+        result = calculate(line)          
+        print("Result:", result)
